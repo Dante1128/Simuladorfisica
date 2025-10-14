70df57ea557ddf0449c3b2b44404b99bafd57f2b
Kristy Rojas <kristinarojas124@gmail.com>
2025-10-13 22:17:30 -0400

    realizacion del modulo de contenido teorico

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('panel_admin/',views.panel_admin, name='panel_admin'),
+    path('gestion-documentos/', views.gestion_documentos, name='gestion_documentos'),
+    path('contenido-teorico/', views.contenido_teorico, name='contenido_teorico'),
+    path('documentos/<int:documento_id>/descargar/', views.descargar_documento, name='descargar_documento'),
+    path('documentos/<int:documento_id>/preview/', views.preview_documento, name='preview_documento'),
    
 ]
