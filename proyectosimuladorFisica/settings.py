3d4850b7d0281d20b1a7f76d033f88c64925e88f
ThaisFlores111 <floresgutierrezthaisdaniela@gmail.com>
2025-10-18 16:35:57 -0400

    feat(superadministrador): detalles finales

--- proyectosimuladorFisica/settings.py
+++ proyectosimuladorFisica/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'simuladordefisica',  
         'USER': 'postgres',         
-        'PASSWORD': '2811',
+        'PASSWORD': 'contra',
         'HOST': 'localhost',        
         'PORT': '5432',              
     }
