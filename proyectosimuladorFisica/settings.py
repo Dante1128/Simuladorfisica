89e919005ae1ff38284a897598f416b984edee18
Kristy Rojas <kristinarojas124@gmail.com>
2025-10-22 00:25:35 -0400

    modulo gestion de contenido

--- proyectosimuladorFisica/settings.py
+++ proyectosimuladorFisica/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Simuladordefisica',  
         'USER': 'postgres',         
-        'PASSWORD': 'miclave123',
+        'PASSWORD': 'contra',
         'HOST': 'localhost',        
         'PORT': '5432',              
     }
