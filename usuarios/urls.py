a2a4fab3a51eee8cc8b65e037dc2c385e5876dcf
Dante1128 <danteyupanqui2811@gmail.com>
2025-11-28 21:50:59 -0400

    feat/icon

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+
+
     # =================================================================
     # SUPERADMINISTRADOR 
     # =================================================================
